25ebbd9d0b89fc9a6532f3aa23bc3c0cef606d94
H4S0 <hasanalic10@gmail.com>
2025-04-24 13:32:08 +0200

    server auth session

--- app/hook/useHasckSession.ts
+++ app/hook/useHasckSession.ts
@@ -3,22 +3,33 @@ import jwt from "jsonwebtoken";
 import {NextResponse} from "next/server";
 import {User} from "@/app/models/User";
 
-export const useHasckSession = async () => {
-    const currectCookies = await cookies();
-    const token = (await currectCookies).get('token')?.value as string;
-
-    if (!token) {
-        return NextResponse.json({ message: 'Invalid token!' });
-    }
-
-    jwt.verify(token, process.env.JWT_SECRET as string);
-    const decode = jwt.decode(token);
-    const parsed = typeof decode === 'string' ? JSON.parse(decode) : decode;
-
-    const user = await User.findById(parsed.id)
-
-    if(!user){
-        console.log('not authenticated')
-    }
-    console.log(user)
+interface SessionResult {
+    user: any | null;
+    authenticated: boolean;
+    error?: string;
 }
+
+export const useHasckServerSession = async (): Promise<SessionResult> => {
+    try {
+        const token = (await cookies()).get("token")?.value;
+
+        if (!token) {
+            return { user: null, authenticated: false, error: "Token not found" };
+        }
+
+        //ovaj decode i parse staviti u jednu funckiju jer se ponavlja na više mjesta
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+        const parsed = typeof decoded === "string" ? JSON.parse(decoded) : decoded;
+
+        const user = await User.findById(parsed.id);
+
+        if (!user) {
+            return { user: null, authenticated: false, error: "User not found" };
+        }
+
+        return { user, authenticated: true };
+    } catch (err) {
+        console.error("Auth error:", err);
+        return { user: null, authenticated: false, error: "Invalid or expired token" };
+    }
+};
