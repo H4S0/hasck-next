b4f1f827cfff39d6c5f91b3e68334657b2cb2f63
H4S0 <hasanalic10@gmail.com>
2025-07-22 15:02:59 +0200

    email template

--- components/template/EmailTemplate.tsx
+++ components/template/EmailTemplate.tsx
@@ -2,12 +2,38 @@ import * as React from 'react';
 
 interface EmailTemplateProps {
   firstName: string;
+  username?: string;
 }
 
-export function EmailTemplate({ firstName }: EmailTemplateProps) {
+export function EmailTemplate({ firstName, username }: EmailTemplateProps) {
   return (
-    <div>
-      <h1>Welcome, {firstName}!</h1>
+    <div
+      style={{
+        fontFamily: 'Arial, sans-serif',
+        padding: '20px',
+        color: '#333',
+      }}
+    >
+      <h1>Welcome aboard, {firstName}!</h1>
+      <p>🎉 Your registration was successful.</p>
+
+      {username && (
+        <p>
+          You can now log in using your username: <strong>{username}</strong>
+        </p>
+      )}
+
+      <p>We are excited to have you as part of our community.</p>
+
+      <p>
+        If you have any questions, feel free to reach out to our support team.
+      </p>
+
+      <p style={{ marginTop: '30px' }}>
+        Best regards,
+        <br />
+        The Team 🚀
+      </p>
     </div>
   );
 }
