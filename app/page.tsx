0950198635e3f7e3e66c00c9f87a2ff16c076b27
H4S0 <hasanalic10@gmail.com>
2025-07-24 01:06:48 +0200

    some fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,9 @@ import { CtaSection } from '@/components/sections/cta-section';
 import { Footer } from '@/components/sections/footer';
 import { HeroSection } from '@/components/sections/hero-section';
 import { getHasckServerSession } from './services/getHasckServerSession';
-import { redirect } from 'next/navigation';
 
 export default async function Page() {
   const { isAuthenticated } = await getHasckServerSession();
-
-  if (!isAuthenticated) {
-    return redirect('/login');
-  }
 
   return (
     <div className="min-h-screen flex flex-col">
